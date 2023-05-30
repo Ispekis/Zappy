c0f3b458f284daccb854385ab8cf8293db040dc0
Vincent Shao <vincent.shao@epitech.eu>
2023-05-30 22:25:18 +0200

    feat[Server]: function that display the usage on the main issue: #4

---
+++ src/server/include/server.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** server
+*/
+
+#ifndef SERVER_H_
+    #define SERVER_H_
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <string.h>
+
+#endif /* !SERVER_H_ */
--- src/server/src/main.c
+++ src/server/src/main.c
@@ -5,10 +5,25 @@
 ** main
 */
 
-#include <stdio.h>
+#include "server.h"
+
+static void show_usage(const char *binary, int fd)
+{
+    dprintf(fd, "USAGE:\t%s -p port -x width -y height -n name1 name2 ... -c clientsNb -f freq\n", binary);
+    dprintf(fd, "\tport\t\tis the port number\n");
+    dprintf(fd, "\twidth\t\tis the width of the world\n");
+    dprintf(fd, "\theight\t\tis the height of the world\n");
+    dprintf(fd, "\tnameX\t\tis the name of the team X\n");
+    dprintf(fd, "\tclientsNb\tis the number of authorized clients per team\n");
+    dprintf(fd, "\tfreq\t\tis the reciprocal of time unit for execution of actions\n");
+}
 
 int main(const int ac, const char **av)
 {
+    if (ac == 2 && strcmp(av[1], "-help") == 0) {
+        show_usage(av[0], STDOUT_FILENO);
+        return 0;
+    }
     printf("Server here !\n");
     return 0;
 }
