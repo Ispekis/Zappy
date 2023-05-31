cc62ff590319c829264d2adeb7dc77748df1f4e4
Vincent Shao <vincent.shao@epitech.eu>
2023-06-01 00:35:38 +0200

    feat[AI]: Displaying usage when needed Also added some file to structure other task in issue Issue: #10

--- src/ai/src/main.py
+++ src/ai/src/main.py
@@ -5,9 +5,24 @@ import sys
 # Include relative path of the source files from the root
 sys.path.append('src/ai/src')
 
-def main():
+from myparser import parse_data
+import usage
+from zappy_ai import run_ai
+from myexeption import CustomException
+
+def main(argv):
+    if len(argv) == 2 and argv[1] == "-help":
+        usage.show_usage(sys.stdout)
+        return 0
+    try:
+        port, name, machine = parse_data(argv)
+    except CustomException as e:
+        print(e, file=sys.stderr)
+
+    run_ai()
+
     print("Ai here !")
     return 0
 
 if __name__ == "__main__":
-    exit(main())
+    exit(main(sys.argv))
---
+++ src/ai/src/myparser.py
@@ -0,0 +1,4 @@
+import getopt
+
+def parse_data(argv):
+    return
