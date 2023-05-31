ecf0c93072f1c5d6d955e23bade7397e4b557cdc
Vincent Shao <vincent.shao@epitech.eu>
2023-06-01 01:02:07 +0200

    feat[AI]: Parsing arguments Using getopt module Issue: #10

--- src/ai/src/main.py
+++ src/ai/src/main.py
@@ -9,6 +9,7 @@ from myparser import parse_data
 import usage
 from zappy_ai import run_ai
 from myexeption import CustomException
+from getopt import GetoptError
 
 def main(argv):
     if len(argv) == 2 and argv[1] == "-help":
@@ -16,12 +17,14 @@ def main(argv):
         return 0
     try:
         port, name, machine = parse_data(argv)
+    except (CustomException, GetoptError) as e:
+        print("Parser: %s" % (e), file=sys.stderr)
+        return 84
+
+    try:
+        run_ai(port, name, machine)
     except CustomException as e:
-        print(e, file=sys.stderr)
-
-    run_ai()
-
-    print("Ai here !")
+        return 84
     return 0
 
 if __name__ == "__main__":
