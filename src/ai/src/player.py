64545a1a1ace2dec7b3b32196f04fb8cc663e136
BRR steven <steven.dong@epitech.eu>
2023-06-05 17:21:51 +0200

    feat[AI]: get the data from the server

---
+++ src/ai/src/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self):
+        self.food = 1260
+        self.linemate = 0
+        self.deraumere = 0
+        self.sibur = 0
+        self.mendiane = 0
+        self.phiras = 0
+        self.thystame = 0
