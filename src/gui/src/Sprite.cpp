941ad17727688865abd73890be2dce64478bb477
Jeras Bertine <jeras.bertine@epitech.eu>
2023-06-06 13:55:33 +0200

    feat[GUI]: implementation of the new class Sprite that will handle the sprites

---
+++ src/gui/src/Sprite.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** Sprite
+*/
+
+#include "Sprite.hpp"
+
+Zappy::Sprite::Sprite()
+{
+}
+
+void Zappy::Sprite::setTexture()
+{
+}
+
+void Zappy::Sprite::setPosition()
+{
+}
+
+void Zappy::Sprite::setScale()
+{
+}
+
+Zappy::Sprite::~Sprite()
+{
+}
