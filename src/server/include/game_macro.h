403c7dc28e9af29299da9218ead52bd23d3ac494
Vincent Shao <vincent.shao@epitech.eu>
2023-06-07 00:58:12 +0200

    feat[Server]: Init map with its position and its resources Issue: #58

---
+++ src/server/include/game_macro.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** zappy_macro
+*/
+
+#ifndef ZAPPY_MACRO_H_
+    #define ZAPPY_MACRO_H_
+    #define FOOD_DENSITY 0.5
+    #define LINEMATE_DENSITY 0.3
+    #define DERAUMERE_DENSITY 0.15
+    #define SIBUR_DENSITY 0.1
+    #define MENDIANE_DENSITY 0.1
+    #define PHIRAS_DENSITY 0.08
+    #define THYSTAME_DENSITY 0.05
+
+
+#endif /* !ZAPPY_MACRO_H_ */
