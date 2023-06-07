1e79f2bb6540b6a32213775672f6b0707660ea73
BRR steven <steven.dong@epitech.eu>
2023-06-07 15:54:08 +0200

    feat[AI]: parse the server response

---
+++ src/ai/src/parsing.py
@@ -0,0 +1,11 @@
+def parseLook(look:str):
+    res:str = look.split(',')
+    return res
+
+def parseInventory(inventory:str):
+    tmp = inventory.split(',')
+    res = [()]
+    for i in range(len(tmp)):
+        v = tmp[i].split()
+        res.append((v[0], v[1]))
+    return res[1:]
