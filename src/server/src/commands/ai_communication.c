abe72927b5665bb64eaf97590ea4fca1bc0486f8
Vincent Shao <vincent.shao@epitech.eu>
2023-06-10 01:48:32 +0200

    feat[Server]: Setup array of function pointing to a variable for ai commands factory Issue: #82

--- src/server/src/commands/ai_communication.c
+++ src/server/src/commands/ai_communication.c
@@ -9,11 +9,20 @@
 #include "macro.h"
 #include "game_macro.h"
 
+static void choose_cmd(char *buffer, node_t *client, server_t server)
+{
+
+}
+
 int do_ai_communication(char *buffer, node_t *client, server_t *server)
 {
-    if (!client->client.is_conn)
+    if (!client->client.is_conn) {
         if (do_ai_first_connect(buffer, client, &server->data) == SUCCESS)
             return SUCCESS;
+    } else {
+            choose_cmd(buffer, client, *server);
+        return SUCCESS;
+    }
     printf("From team\n");
     return FAILURE;
 }
---
+++ src/server/src/commands/ai_interact.c
@@ -0,0 +1,7 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** ai_interact
+*/
+
