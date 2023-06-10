173c5450adbc48ea0c3e5ee0bdd6a1cfa71dc967
Vincent Shao <vincent.shao@epitech.eu>
2023-06-10 22:38:21 +0200

    feat[Server]: Implementing inventory command and setting others commands an unsed attributes Issue: #87

--- src/server/src/commands/ai_info.c
+++ src/server/src/commands/ai_info.c
@@ -7,14 +7,21 @@
 
 #include "server.h"
 
-void ai_cmd_look(node_t *client, data_t *data, char **params)
+void ai_cmd_look(node_t *client, data_t *data,
+char **params __attribute__((unused)))
 {
 
 }
 
-void ai_cmd_inventory(node_t *client, data_t *data, char **params)
+void ai_cmd_inventory(node_t *client, data_t *data,
+char **params __attribute__((unused)))
 {
+    inventory_t inv = client->client.inventory;
 
+    dprintf(client->client.fd, "[ food %i, linemate %i, deraumere %i, \
+sibur %i, mendiane %i, phiras %i, thystame %i ]\n", inv.food.quantity,
+    inv.linemate.quantity, inv.deraumere.quantity, inv.sibur.quantity,
+    inv.mendiane.quantity, inv.phiras.quantity, inv.thystame.quantity);
 }
 
 void ai_cmd_broadcast(node_t *client, data_t *data, char **params)
