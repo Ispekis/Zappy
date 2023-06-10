2483c5b73dc0ea2ed79d862b742cab77fc5682dd
Jouzep <joseph.yu@epitech.eu>
2023-06-10 19:04:13 +0200

    feat[GUI]: adding Team/Player/Time class #57

---
+++ src/gui/src/Map/Player.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** Player
+*/
+
+#ifndef PLAYER_HPP_
+#define PLAYER_HPP_
+
+namespace Zappy {
+    class Player {
+        public:
+            Player();
+            ~Player();
+    
+        protected:
+        private:
+    };
+}
+
+#endif /* !PLAYER_HPP_ */
---
+++ src/gui/src/Map/TimeUnit.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** TimeUnit
+*/
+
+#ifndef TIMEUNIT_HPP_
+#define TIMEUNIT_HPP_
+
+namespace Zappy {
+    class TimeUnit {
+        public:
+            TimeUnit();
+            ~TimeUnit();
+    
+        protected:
+        private:
+    };
+}
+
+#endif /* !TIMEUNIT_HPP_ */
