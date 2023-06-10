822b1f579c65a5feb283a78ea05bd2ecf93f489e
Jouzep <joseph.yu@epitech.eu>
2023-06-10 19:04:13 +0200

    feat[GUI]: adding Team/Player/Time class #57

---
+++ src/gui/src/Map/TimeUnit.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** TimeUnit
+*/
+
+#include "TimeUnit.hpp"
+
+Zappy::TimeUnit::TimeUnit()
+{
+}
+
+Zappy::TimeUnit::~TimeUnit()
+{
+}
