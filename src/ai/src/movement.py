bfef4d45a6ce2ece405d394650b553cb78aa20b0
zvillon <zhg.villon@gmail.com>
2023-06-12 18:12:37 +0200

    feat[AI]: Rebase ai branch to movement branch

--- src/ai/src/movement.py
+++ src/ai/src/movement.py
@@ -2,12 +2,30 @@ import random
 from macro import *
 import sys
 
+LEVEL1 = [("linemate", 1)]
+LEVEL2 = [("linemate", 1), ("deraumere", 1), ("sibur", 1)]
 
 class Movement:
     def __init__(self):
-        self.movementList = [FORWARD, FORWARD, FORWARD, RIGHT, LEFT]
-       
-    def handleMovement(self):
-        tmp = random.choice(self.movementList)
-        print(f"{tmp}", file=sys.stderr)
+        self.movementList:list = [FORWARD, FORWARD, RIGHT, LEFT]
+        self.preMove:str = None
+    
+    def handleObjectives(self, objective:int) -> int:
+        return 0
+    
+    def getActionCost(self, frequency:list) -> int:
+        return 0
+    
+    def handleMovement(self, sight:str) -> str:
+        if (self.preMove != None):
+            tmp = self.preMove
+            self.preMove = None
+            return tmp
+        liste:list = []
+        for i in range(len(sight)):
+            if sight[i].find(LEVEL1[0][0]) != -1:
+                liste.append(i)
+        if (len(liste) != 0):
+            return self.handleObjectives()
+        tmp:str = random.choice(self.movementList)
         return tmp
