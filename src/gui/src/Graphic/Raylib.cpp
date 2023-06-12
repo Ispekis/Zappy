651b39cf4b25482d307978947a622edc924a1867
Jouzep <joseph.yu@epitech.eu>
2023-06-12 10:46:31 +0200

    feat[GUI]: Initalise 3D area with simple box #41

--- src/gui/src/Graphic/Raylib.cpp
+++ src/gui/src/Graphic/Raylib.cpp
@@ -13,6 +13,12 @@ Zappy::Raylib::Raylib(int screenWidth, int screenHeight, std::string title)
     if (!IsWindowReady())
         throw Error("RayLib", "Error Init Window");
     SetTargetFPS(60);
+    _camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // Camera position
+    _camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    _camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    _camera.fovy = 45.0f;                                // Camera field-of-view Y
+    _camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 }
 
 void Zappy::Raylib::setData(std::shared_ptr<Data> data)
@@ -34,10 +40,22 @@ void Zappy::Raylib::event()
 
 }
 
+void Zappy::Raylib::drawMap()
+{
+    DrawCube((Vector3){ 0.0f, 0.0f, 0.0f }, 2.0f, 2.0f, 2.0f, RED);
+    DrawCubeWires((Vector3){ 0.0f, 0.0f, 0.0f }, 2.0f, 2.0f, 2.0f, MAROON);
+    DrawGrid(10, 1.0f);
+}
+
 void Zappy::Raylib::draw()
 {
     BeginDrawing();
     ClearBackground(RAYWHITE);
+    BeginMode3D(_camera);
+
+    drawMap();
+
+    EndMode3D();
     EndDrawing();
 }
 
