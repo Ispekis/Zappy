f58e7963dd4ac5637411072f1e9ce02f2b133c00
BRR steven <steven.dong@epitech.eu>
2023-06-13 13:51:00 +0200

    feat[AI]: set objectif list

--- src/ai/src/level.py
+++ src/ai/src/level.py
@@ -5,13 +5,13 @@ class level:
         self.levels = parseLevel("/home/bsteven/delivery/TEK2/YEP/B-YEP-400-PAR-4-1-zappy-vincent.shao/src/ai/level.json")
 
     def setObj(self, level):
-        return self.levels[level - 1]
+        return self.levels[str(level)]
 
 def parseLevel(filename):
     with open(filename) as file:
         data = json.load(file)
         parsed_list = {}
         for key, value in data.items():
-            parsed_list[key] = {k: v for k, v in value.items()}
+            parsed_list[str(key)] = {k: v for k, v in value.items()}
             print(parsed_list)
         return parsed_list
