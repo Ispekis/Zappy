2ea922c76919d919424c5625b105b7978a756479
Jouzep <joseph.yu@epitech.eu>
2023-06-13 15:30:29 +0200

    feaŧ[GUI]: adding background menu

---
+++ test.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** test
+*/
+
+#include <iostream>
+
+int main()
+{
+    bool e = false;
+
+    e = !e;
+    std::cout << e << std::endl;
+}
