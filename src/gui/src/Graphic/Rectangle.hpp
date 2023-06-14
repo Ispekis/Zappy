edf6e79db586f7f225410af8a4a6e6b18b0b9bb0
Jeras Bertine <jeras.bertine@epitech.eu>
2023-06-14 14:41:27 +0200

    feat[GUI]: creation of three buttons for play, settings et quit

--- src/gui/src/Graphic/Rectangle.hpp
+++ src/gui/src/Graphic/Rectangle.hpp
@@ -32,6 +32,20 @@ namespace Zappy {
             void drawRect(float width, float height, Vector2 position);
 
             /**
+             * @brief Set the Texture object
+             * 
+             * @param text 
+             */
+            void setTexture(Texture2D text);
+
+            /**
+             * @brief Get the Rect object
+             * 
+             * @param position 
+             * @return Vector2 
+             */
+            Rectangle getRect();
+            /**
              * @brief Destroy the Rect object
              * 
              */
@@ -40,6 +54,7 @@ namespace Zappy {
         protected:
         private:
             Texture2D _text;
+            Rectangle _dest;
     };
 }
 
