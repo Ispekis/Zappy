ad9cb4d8762a29b4531dc939a3b14f8b809f49db
David <david1.lin@epitech.eu>
2023-06-15 18:31:36 +0200

    feat[GUI] item mouvement

--- src/gui/src/Graphic/Sprite.cpp
+++ src/gui/src/Graphic/Sprite.cpp
@@ -23,6 +23,11 @@ Zappy::Sprite::Sprite(Texture2D top, Texture2D side, Texture2D bot, Shader shade
     _shader = std::make_shared<Shaders>(shader);
 }
 
+Zappy::Sprite::Sprite(Texture2D item)
+{
+    _item = item;
+}
+
 void Zappy::Sprite::setTexture()
 {
 }
@@ -84,11 +89,13 @@ void Zappy::Sprite::drawBlockTexture(Vector3 position, Vector3 size, Color color
         rlEnd();
 
     rlSetTexture(_top.id);
-        rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x - width/2, y + height/2, z - length/2);
-        rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x - width/2, y + height/2, z + length/2);
-        rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x + width/2, y + height/2, z + length/2);
-        rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x + width/2, y + height/2, z - length/2);
-    rlEnd();
+        rlBegin(RL_QUADS);
+            rlColor4ub(color.r, color.g, color.b, color.a);
+                rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x - width/2, y + height/2, z - length/2);
+                rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x - width/2, y + height/2, z + length/2);
+                rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x + width/2, y + height/2, z + length/2);
+                rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x + width/2, y + height/2, z - length/2);
+
 
     rlSetTexture(_bot.id);
         rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x - width/2, y - height/2, z - length/2);
@@ -111,7 +118,6 @@ void Zappy::Sprite::drawWaterTexture(Vector3 position, Vector3 size, Color color
     
     if (_shader != nullptr) {
         seconds += GetFrameTime();
-        Rotation -= 3.0f;
         SetShaderValue(_shader->_shader, _shader->getSecondLock(), &seconds, RL_SHADER_UNIFORM_FLOAT);
         BeginShaderMode(_shader->_shader);
     }
@@ -122,9 +128,65 @@ void Zappy::Sprite::drawWaterTexture(Vector3 position, Vector3 size, Color color
                 rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x - width/2, y + height/2, z + length/2);
                 rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x + width/2, y + height/2, z + length/2);
                 rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x + width/2, y + height/2, z - length/2);
-
         rlEnd();
     if (_shader != nullptr)
         EndShaderMode();
+    rlSetTexture(_bot.id);
+        (1.0f, 1.0f); rlVertex3f(x - width/2, y - height/2, z - length/2);
+        (0.0f, 1.0f); rlVertex3f(x + width/2, y - height/2, z - length/2);
+        (0.0f, 0.0f); rlVertex3f(x + width/2, y - height/2, z + length/2);
+        (1.0f, 0.0f); rlVertex3f(x - width/2, y - height/2, z + length/2);
+    rlEnd();
     rlSetTexture(0);
+}
+
+void Zappy::Sprite::drawItemTexture(Vector3 position, Vector3 size, Color color)
+{
+    float x = position.x / 100;
+    float y = position.y / 100;
+    float z = position.z / 100;
+
+    float width = size.x;
+    float height = size.y;
+
+    itemRotation -= 0.01f;;
+    if (itemBounce < height - (height / 8)  && ret == false) {
+        itemBounce += 0.0001f;
+        if (itemBounce > height - (height / 8) )
+            ret = true;
+    }
+    if (itemBounce > -height / 8 && ret == true) {
+        itemBounce -= 0.0001f;
+       if (itemBounce < -height / 8)
+            ret = false;
+    }
+
+    rlPushMatrix();
+    rlTranslatef(x * 100, y * 100, z * 100);
+    rlRotatef(itemRotation, 0.0f, 1.0f, 0.0f);
+    rlSetTexture(_item.id);
+        rlBegin(RL_QUADS);
+            rlColor4ub(color.r, color.g, color.b, color.a);
+                rlTexCoord2f(0.0f, 0.0f); rlVertex3f((x - 0.075) + width/3, y + height/3 + itemBounce, 0);
+                rlTexCoord2f(1.0f, 0.0f); rlVertex3f((x - 0.075) - width/3, y + height/3 + itemBounce, 0);
+                rlTexCoord2f(1.0f, 1.0f); rlVertex3f((x - 0.075) - width/3, y - height/3 + itemBounce, 0);
+                rlTexCoord2f(0.0f, 1.0f); rlVertex3f((x - 0.075) + width/3, y - height/3 + itemBounce, 0);
+
+                rlTexCoord2f(0.0f, 0.0f); rlVertex3f((x + 0.075)  + width/3, y + height/3 + itemBounce, 0);
+                rlTexCoord2f(0.0f, 1.0f); rlVertex3f((x + 0.075)  + width/3, y - height/3 + itemBounce, 0);
+                rlTexCoord2f(1.0f, 1.0f); rlVertex3f((x + 0.075)  - width/3, y - height/3 + itemBounce, 0);
+                rlTexCoord2f(1.0f, 0.0f); rlVertex3f((x + 0.075)  - width/3, y + height/3 + itemBounce, 0);
+
+                // rlTexCoord2f(0.0f, 0.0f); rlVertex3f((x - -0.075) + width/3, y + height/3 + bounce, 0.07);
+                // rlTexCoord2f(1.0f, 0.0f); rlVertex3f((x - -0.075) - width/3, y + height/3 + bounce, 0.07);
+                // rlTexCoord2f(1.0f, 1.0f); rlVertex3f((x - -0.075) - width/3, y - height/3 + bounce, 0.07);
+                // rlTexCoord2f(0.0f, 1.0f); rlVertex3f((x - -0.075) + width/3, y - height/3 + bounce, 0.07);
+
+                // rlTexCoord2f(0.0f, 0.0f); rlVertex3f((x + -0.075) + width/3, y + height/3 + bounce, -0.07);
+                // rlTexCoord2f(0.0f, 1.0f); rlVertex3f((x + -0.075) + width/3, y - height/3 + bounce, -0.07);
+                // rlTexCoord2f(1.0f, 1.0f); rlVertex3f((x + -0.075) - width/3, y - height/3 + bounce, -0.07);
+                // rlTexCoord2f(1.0f, 0.0f); rlVertex3f((x + -0.075) - width/3, y + height/3 + bounce, -0.07);
+    rlEnd();
+    rlSetTexture(0);
+    rlPopMatrix();
 }
