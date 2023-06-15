2b8263d6837812aa503c2c5839b22cb318d82e95
David <david1.lin@epitech.eu>
2023-06-15 16:08:03 +0200

    feat[GUI]: shader for waterblock

--- src/gui/src/Graphic/Sprite.cpp
+++ src/gui/src/Graphic/Sprite.cpp
@@ -12,6 +12,15 @@ Zappy::Sprite::Sprite(Texture2D top, Texture2D side, Texture2D bot)
     _top = top;
     _side = side;
     _bot = bot;
+    _shader = nullptr;
+}
+
+Zappy::Sprite::Sprite(Texture2D top, Texture2D side, Texture2D bot, Shader shader)
+{
+    _top = top;
+    _side = side;
+    _bot = bot;
+    _shader = std::make_shared<Shaders>(shader);
 }
 
 void Zappy::Sprite::setTexture()
@@ -88,5 +97,34 @@ void Zappy::Sprite::drawBlockTexture(Vector3 position, Vector3 size, Color color
         rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x - width/2, y - height/2, z + length/2);
     rlEnd();
 
+    rlSetTexture(0);
+}
+
+void Zappy::Sprite::drawWaterTexture(Vector3 position, Vector3 size, Color color)
+{
+    float x = position.x;
+    float y = position.y;
+    float z = position.z;
+    float width = size.x;
+    float height = size.y;
+    float length = size.z;
+    
+    if (_shader != nullptr) {
+        seconds += GetFrameTime();
+        Rotation -= 3.0f;
+        SetShaderValue(_shader->_shader, _shader->getSecondLock(), &seconds, RL_SHADER_UNIFORM_FLOAT);
+        BeginShaderMode(_shader->_shader);
+    }
+    rlSetTexture(_top.id);
+        rlBegin(RL_QUADS);
+            rlColor4ub(color.r, color.g, color.b, color.a);
+                rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x - width/2, y + height/2, z - length/2);
+                rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x - width/2, y + height/2, z + length/2);
+                rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x + width/2, y + height/2, z + length/2);
+                rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x + width/2, y + height/2, z - length/2);
+
+        rlEnd();
+    if (_shader != nullptr)
+        EndShaderMode();
     rlSetTexture(0);
 }
