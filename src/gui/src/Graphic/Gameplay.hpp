d4afc18304ba751caf4d08a503560df5a7e51576
Jouzep <joseph.yu@epitech.eu>
2023-06-17 14:28:47 +0200

    feat[GUI]: camera 3D wrapper cpp

---
+++ src/gui/src/Graphic/Gameplay.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-PAR-4-1-zappy-vincent.shao
+** File description:
+** Gameplay
+*/
+
+#ifndef GAMEPLAY_HPP_
+#define GAMEPLAY_HPP_
+
+namespace Zappy {
+    class Gameplay {
+        public:
+            Gameplay();
+            ~Gameplay();
+
+            void drawGameplay();
+
+        protected:
+        private:
+    };
+}
+
+#endif /* !GAMEPLAY_HPP_ */
