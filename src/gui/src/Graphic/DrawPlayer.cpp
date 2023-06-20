e3bffe994f3a76ab00d4fb5547a9d5c40004edda
David <david1.lin@epitech.eu>
2023-06-20 17:44:51 +0200

    feat[GUI] loop for different player action

--- src/gui/src/Graphic/DrawPlayer.cpp
+++ src/gui/src/Graphic/DrawPlayer.cpp
@@ -16,12 +16,33 @@ Zappy::DrawPlayer::~DrawPlayer()
 {
 }
 
+void Zappy::DrawPlayer::setData(std::shared_ptr<Data> data)
+{
+    std::shared_ptr<Data> tmp(data, data.get());
+    _data = tmp;
+}
+
 void Zappy::DrawPlayer::setModel()
 {
     _model.insert({"babyking", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/babyking.png")});
+    _model.insert({"brennsou", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/brennsou.png")});
+    _model.insert({"girl", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/girl.png")});
+    _model.insert({"girl1", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/girl1.png")});
+    _model.insert({"kdd", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/kdd.png")});
+    _model.insert({"nezuko", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/nezuko.png")});
+    _model.insert({"nweak", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/nweak.png")});
+    _model.insert({"spiderman", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/spiderman.png")});    
+    _model.insert({"steve", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/steve.png")});    
+    _model.insert({"zirnox", std::make_shared<MyModel>("src/gui/assets/skin/player_model.glb", 3, "src/gui/assets/skin/zirnox.png")});    
 }
 
 void Zappy::DrawPlayer::draw()
 {
-    _model["babyking"]->draw(Vector3 { 0.0f, 3.0f, 0.0f}, 1.5f);    
+    _model["kdd"]->draw(Vector3 { -10.0f, 3.0f, 0.0f}, 3);
+    _model["nweak"]->draw(Vector3 { -5.0f, 3.0f, 0.0f}, 3);
+    _model["nezuko"]->draw(Vector3 { 0.0f, 3.0f, 0.0f}, 3);
+    _model["brennsou"]->draw(Vector3 { 10.0f, 3.0f, 0.0f}, 3);
+    _model["girl1"]->draw(Vector3 { 15.0f, 3.0f, 0.0f}, 3);
+    _model["girl"]->draw(Vector3 { 5.0f, 3.0f, 0.0f}, 3);
 }
+
