2bcae1c02cbefab5f214618ec2633833fc628b28
Vincent Shao <vincent.shao@epitech.eu>
2023-06-21 10:48:28 +0200

    feat[Server]: Correcting memleaks on ai look cmd, fix warning in ai_interact.c

--- src/server/src/commands/ai_interact.c
+++ src/server/src/commands/ai_interact.c
@@ -58,7 +58,7 @@ void ai_cmd_set_object(node_t *client, data_t *data, char **params)
 void ai_cmd_incantation(node_t *client, data_t *data,
 char **params __attribute__((unused)))
 {
-    if (can_elevate(client, &data->map)
+    if (can_elevate(client, data->map)
     && client->client.level < MAX_LEVEL) {
         dprintf(client->client.fd, "Elevation underway\n");
         set_cooldown_in_nanosec(client,
