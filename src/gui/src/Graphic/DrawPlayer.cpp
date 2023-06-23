fea5ebf8dea62e0cfcc51e982899679aa5ae7d2a
David <david1.lin@epitech.eu>
2023-06-23 23:37:33 +0200

    feat[GUI]: player selection

--- src/gui/src/Graphic/DrawPlayer.cpp
+++ src/gui/src/Graphic/DrawPlayer.cpp
@@ -20,9 +20,6 @@ void Zappy::DrawPlayer::setData(std::shared_ptr<Data> data)
 {
     std::shared_ptr<Data> tmp(data, data.get());
     _data = tmp;
-    float size = _data->_gameData._tileSize * 3;
-    for (auto &element: _model)
-        element.second->setSize(size);
 }
 
 static float getRotationAngle(Zappy::Orientation orientation, float actualRotation)
@@ -81,9 +78,6 @@ void Zappy::DrawPlayer::draw(raylib::Camera &camera)
         } else if (tmp == true && player->_selected == true)
             player->_selected = false;
     }
-    // _model["kdd"]->draw((Vector3){0, 10, 0},  0, size);
-    // _model["kdd"]->drawSelectedPlayer(size);
-
 }
 
 void Zappy::DrawPlayer::drawTeamText(Vector3 pos, std::shared_ptr<Player> player, float size, std::string team)
