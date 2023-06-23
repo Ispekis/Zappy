8b9d8a59abfe6d19def54dc52ee3922b01f66b65
David <david1.lin@epitech.eu>
2023-06-23 22:06:03 +0200

    feat[GUI] start player selection

--- src/gui/src/Graphic/MyModel.cpp
+++ src/gui/src/Graphic/MyModel.cpp
@@ -10,6 +10,8 @@
 Zappy::MyModel::MyModel(std::string path, unsigned int animsCount, std::string texture): _animation(LoadModelAnimations(path.c_str(), &animsCount)), _model(path.c_str()), _texture(texture.c_str())
 {
     SetMaterialTexture(&_model.materials[0], MATERIAL_MAP_DIFFUSE, _texture);
+    _boundingBox = GetMeshBoundingBox(_model.meshes[0]);
+    // setBoundingBox();
 }
 
 Zappy::MyModel::~MyModel()
@@ -21,22 +23,33 @@ void Zappy::MyModel::setCamera(raylib::Camera3D camera)
     _camera = camera;
 }
 
-static double degreesToRadians(double degrees) {
-    return degrees * M_PI / 180.0;
-}
-
-bool Zappy::MyModel::drawSelectedPlayer(Vector3 position, float size, float rotation)
+void Zappy::MyModel::setSize(float size)
 {
-    _boundingBox = { (Vector3){ position.x - size * 0.235, position.y, position.z - size * 0.235 },
-                    (Vector3){ position.x + size * 0.235, position.y + size, position.z + size * 0.235 } };
-
-    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-        if (GetRayCollisionBox(GetMouseRay(GetMousePosition(), _camera), _boundingBox).hit)
-            return true;
-    return false;
+    _size = _size;
 }
 
-void Zappy::MyModel::draw(Vector3 pose, float orientation, std::size_t size, bool selected)
+// void Zappy::MyModel::setBoundingBox()
+// {
+//     _boundingBox.min = Vector3 { _boundingBox.min.x * _size, _boundingBox.min.y * _size, _boundingBox.min.z * _size };
+//     _boundingBox.max = Vector3 { _boundingBox.max.x * _size, _boundingBox.max.y * _size, _boundingBox.max.z * _size };
+// }
+
+void Zappy::MyModel::drawSelectedPlayer(float size)
+{
+    _boundingBox.min = Vector3 { _boundingBox.min.x * size, _boundingBox.min.y * size, _boundingBox.min.z * size };
+    _boundingBox.max = Vector3 { _boundingBox.max.x * size, _boundingBox.max.y * size, _boundingBox.max.z * size };
+
+    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+        if (GetRayCollisionBox(GetMouseRay(GetMousePosition(), _camera), _boundingBox).hit)
+            _playerSelected = true;
+        else
+            _playerSelected = false;
+    }
+    if (_playerSelected)
+        DrawBoundingBox(_boundingBox, RED);
+}
+
+void Zappy::MyModel::draw(Vector3 pose, float orientation, std::size_t size)
 {
     if (selected)
         DrawBoundingBox(_boundingBox, RED);
