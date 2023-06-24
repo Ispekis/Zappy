0a8143c9a6700e18f5f17c8c88ca83d97df33030
BRR steven <steven.dong@epitech.eu>
2023-06-24 23:25:59 +0200

    fix [AI]: recv crash

--- src/ai/src/zappy_ai.py
+++ src/ai/src/zappy_ai.py
@@ -3,16 +3,19 @@ import myexception
 import socket
 import sys
 from parsing import *
-from player import Player
+from player import Player, push
 from movement import Movement
 from items import Items
 from macro import *
-from level import levelUp
+from levelup import *
 from fork import *
+from utils import *
+from broadcast import *
 
 class AI:
     def __init__(self, port:str, machine:str, name:str) -> None:
-        """Run the AI script
+        """
+        Run the AI script
 
         Args:
             port (int): Port parameter
@@ -34,61 +37,67 @@ class AI:
         self.move = Movement(self.client_socket, self.player.item_needed)
 
     def setPlayer(self, name:str) -> None:
+        """
+        Set up player's information for the object
+
+        Args:
+            name (str): name of the player
+        """
         recv_data = self.client_socket.recv(1024)
         print(recv_data.decode(), end="")
         rcv_data = self.client_socket.recv(1024)
         tmp = rcv_data.decode().split("\n")
         map_size = tmp[1].split(" ")
         try:
-            self.client_socket.send((f'Broadcast {name}').encode())
-            self.player = Player(tmp[0], (map_size[0], map_size[1]), 1)
+            # self.client_socket.send((f'Broadcast {name}').encode())
+            self.player = Player(int(tmp[0]), 1, name)
         except IndexError:
             print(f"Error while creating the player, check if team name is correct", file=sys.stderr)
 
     def rcvServerResponse(self) -> None:
-        """Set the sight, invetory and update the team slot free
         """
-
+        Set the sight, invetory and update the team slot free
+        """
         self.client_socket.send(("Look\n").encode())
-        self.player.sight = parseLook(self.client_socket.recv(1024).decode()[2:-2])
+        self.player.sight = parseLook(rcvFromatter(self.client_socket)[2:-2])
         # print(f'sight = {self.player.sight}')
+        self.client_socket.send(("Inventory\n").encode())
+        self.player.inventory = parseInventory(rcvFromatter(self.client_socket)[2:-2])
         # print(f'inventory = {self.player.inventory}')
         self.client_socket.send(("Connect_nbr\n").encode())
-        self.player.nb_player = updateNbPlayer(self.client_socket.recv(1024).decode())
+        self.player.nb_player = int(rcvFromatter(self.client_socket))
         # print(f'nbplayer = {self.player.nb_player}')
-        self.client_socket.send(("Inventory\n").encode())
-        self.player.inventory = parseInventory(self.client_socket.recv(1024).decode()[2:-2])
-
-    def level_up(self) -> None:
-        if levelUp(self.player.obj_list, self.player.sight[0]):
-            self.client_socket.send(("Incantation\n").encode())
-            # if self.client_socket.recv(1024).decode() != "ko":
-            print(self.client_socket.recv(1024).decode())
-
-    def push(self) -> None:
-        if self.player.multiplePlayerTile():
-            self.client_socket.send(("push\n").encode())
-            rcv_data = self.client_socket.recv(1024)
 
     def reproduction(self) -> None:
         if check_if_need_fork(self.player, self.player.sight):
             self.client_socket.send("fork\n".encode())
-            rcv_data = self.client_socket.recv(1024)
+            rcv_data = rcvFromatter(self.client_socket)
 
     def playerAction(self) -> None:
-        self.itemHandling.takeItem(self.player.sight, self.player.item_needed, self.player.needList, self.player.inventory)
-        self.push()
-        self.level_up()
-        self.reproduction()
-        self.itemHandling.needsFood(self.player.inventory, self.player.needList)
-        self.move.handleMovement(self.player.sight, self.player.needList)
+        levelUp(self.player, self.client_socket)
+        if self.player.attitude != INCANTATION:
+            self.itemHandling.needsFood(self.player.inventory, self.player.needList)
+            self.itemHandling.takeItem(self.player.sight, self.player.item_needed, self.player.needList, self.player.inventory)
+            self.move.handleMovement(self.player.sight, self.player.needList)
+            push(self.player, self.client_socket)
+            self.reproduction()
+            broadcast(self.player, self.client_socket)
+
 
     def run_ai(self) -> int:
+        """
+        Main loop of the program, sending and receiving informations to/from the server.
+
+        Returns:
+            int: _description_
+        """
         try:
             while True:
+                # rcv_value = rcvFromatter(self.client_socket)
+                # if (rcv_value != None):
+                #     analyse_broadcast(self.player, rcv_value)
                 self.rcvServerResponse()
                 self.playerAction()
-                print(self.player.inventory)
         except KeyboardInterrupt:
             return SUCCESS
         except BrokenPipeError:
